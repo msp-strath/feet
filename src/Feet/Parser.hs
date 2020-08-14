7c4a7199a50174d5d7b9d3b76fdc0b7977d1eba8
Fredrik Nordvall Forsberg <fredrik.nordvall-forsberg@strath.ac.uk>
2020-08-14 19:00:32 +0100

    factor out Thin from the parser

--- src/Feet/Parser.hs
+++ src/Feet/Parser.hs
@@ -9,6 +9,7 @@ import Data.List
 import Data.Char
 
 import Feet.Syntax
+import Feet.Thinning
 import Feet.Frontend
 
 newtype DBP x = DBP {dbp :: [String] -> String -> [(x, String)]}
@@ -197,37 +198,3 @@ pGo :: DBP x -> String -> [x]
 pGo p s = do
   (x, "") <- dbp p [] s
   return x
-
-class Thin t where
-  (<^>) :: t -> Integer -> t
-
-instance Thin Int where
-  i <^> th = case (th `mod` 2, th `div` 2) of
-    (0, th) -> 1 + (i <^> th)
-    (1, th) -> case i of
-      0 -> 0
-      _ -> 1 + ((i - 1) <^> th)
-
-instance Thin x => Thin (Abs x) where
-  Abs t <^> th = Abs (t <^> (2 * th + 1))
-
-instance Thin Chk where
-  Inf e <^> th = Inf (e <^> th)
-  Pi s t <^> th = Pi (s <^> th) (t <^> th)
-  Sg s t <^> th = Pi (s <^> th) (t <^> th)
-  List t <^> th = List (t <^> th)
-  Thinning a b c <^> th = Thinning (a <^> th) (b <^> th) (c <^> th)
-  Lam t <^> th = Lam (t <^> th)
-  Pair s t <^> th = Pair (s <^> th) (t <^> th)
-  Single t <^> th = Single (t <^> th)
-  (s :++: t) <^> th = (s <^> th) :++: (t <^> th)
-  Map f t <^> th = Map (f <^> th) (t <^> th)
-  (s :*: t) <^> th = (s <^> th) :*: (t <^> th)
-  GInv t <^> th = GInv (t <^> th)
-  t <^> th = t -- should only be used at arity 0  
-  
-instance Thin Syn where
-  (t ::: y) <^> th = (t <^> th) ::: (y <^> th)
-  Var i <^> th = Var (i <^> th)
-  (f :$: s) <^> th = (f <^> th) :$: (s <^> th)
-  ListRec p n c e <^> th = ListRec (p <^> th) (n <^> th) (c <^> th) (e <^> th)
